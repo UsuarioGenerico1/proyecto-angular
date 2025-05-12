9a88dc50b802e4c72b34835643413cbc07843a11
How Good <quebien2002@gmail.com>
2025-05-12 12:45:38 -0500

    creacion de home y login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { CrudForoComponent } from './components/crud-foro/crud-foro.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'login',component:LoginComponent},
+    {path:'home',component:HomeComponent},
+    {path:'foro',component:CrudForoComponent},
+    {path:'',redirectTo:'/home',pathMatch:'full'}
+];
