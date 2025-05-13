a131686587d1f94a9e43ed0aca6d5e0a2d690608
Xhope <morabryan322@gmail.com>
2025-05-13 00:30:39 -0500

    instalacion angular material

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { CrudForoComponent } from './components/crud-foro/crud-foro.component';
+import { CrudReportesProblemasComponent } from './components/crud-reportes-problemas/crud-reportes-problemas.component';
 
 export const routes: Routes = [
     {path:'login',component:LoginComponent},
     {path:'home',component:HomeComponent},
     {path:'foro',component:CrudForoComponent},
+    {path: 'reportes', component: CrudReportesProblemasComponent, title: 'Reportes de Problemas'},
     {path:'',redirectTo:'/home',pathMatch:'full'}
 ];
---
+++ src/app/components/crud-reportes-problemas/crud-reportes-problemas.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-reportes-problemas',
+  imports: [],
+  templateUrl: './crud-reportes-problemas.component.html',
+})
+export class CrudReportesProblemasComponent { }
