5991e7ffcb9b5e8a1d0ae0db46326c7c44e1887c
Xhope <morabryan322@gmail.com>
2025-05-15 00:35:41 -0500

    feat: creación del servicio de reportes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,13 @@ import { CrudForoComponent } from './components/crud-foro/crud-foro.component';
 import { CrudReportesProblemasComponent } from './components/crud-reportes-problemas/crud-reportes-problemas.component';
 
 export const routes: Routes = [
-    {path:'login',component:LoginComponent},
-    {path:'home',component:HomeComponent},
-    {path:'foro',component:CrudForoComponent},
-    {path: 'reportes', component: CrudReportesProblemasComponent, title: 'Reportes de Problemas'},
-    {path:'',redirectTo:'/home',pathMatch:'full'}
+  { path: 'home', title: 'Home', component: HomeComponent },
+  { path: 'login', title: 'Login', component: LoginComponent },
+  { path: 'foro', title: 'Foro', component: CrudForoComponent },
+  {
+    path: 'reportes',
+    title: 'Reportes de Problemas',
+    component: CrudReportesProblemasComponent,
+  },
+  { path: '**', redirectTo: 'home' },
 ];
--- src/app/services/reporte-problemas.service.ts
+++ src/app/services/reporte-problemas.service.ts
@@ -1,10 +1,53 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+import { Reportes } from '../models/reportes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReporteProblemasService {
+  private jsonUrl = 'http://localhost:3000/reportes'; //ruta del archivo json
 
-  constructor() { }
+  constructor(private httpclient: HttpClient) {}
 
+  getReportes(): Observable<Reportes[]> {
+    return this.httpclient.get<Reportes[]>(this.jsonUrl);
+    // .get<{ peliculas: Pelicula[] }>(this.jsonUrl)
+    // .pipe(map((data) => data.peliculas));
+  }
+
+  getReporteSearch(buscar?: string): Observable<Reportes[]> {
+    return this.httpclient.get<Reportes[]>(this.jsonUrl).pipe(
+      map((reportes) =>
+        reportes.filter(
+          (reportes) =>
+            (buscar
+              ? reportes.titulo.toLowerCase().includes(buscar.toLowerCase())
+              : true) || //this works :D
+            (buscar
+              ? reportes.categoria.toLowerCase().includes(buscar.toLowerCase())
+              : true)
+        )
+      )
+    );
+  }
+
+  //agregar reportes
+  addMovie(reporte: Reportes): Observable<Reportes> {
+    return this.httpclient.post<Reportes>(this.jsonUrl, reporte);
+  }
+
+  //editar reportes
+
+  editMovie(reporte: Reportes): Observable<Reportes> {
+    let urlReporte = `${this.jsonUrl}/${reporte.id}`;
+    return this.httpclient.put<Reportes>(urlReporte, reporte);
+  }
+
+  //eliminar reportes
+  deleteMovie(reporte: Reportes): Observable<void> {
+    let urlReporte = `${this.jsonUrl}/${reporte.id}`;
+    return this.httpclient.delete<void>(urlReporte);
+  }
 }
