0f2d174123c67f4a0e701f4730effb8b5695d464
Omar Yagual <its.oyaza20@hotmail.com>
2025-05-14 20:15:09 -0500

    Creacion de Componente y Servicio de CRUD-USUARIO

---
+++ src/app/components/crud-usuario/crud-usuario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-usuario',
+  imports: [],
+  templateUrl: './crud-usuario.component.html',
+  styleUrl: './crud-usuario.component.css'
+})
+export class CrudUsuarioComponent {
+
+}
