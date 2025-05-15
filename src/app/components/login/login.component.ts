068c0fae204ffd5c91af8ae0878aec31e444cf82
Omar Yagual <its.oyaza20@hotmail.com>
2025-05-14 19:52:39 -0500

    Creacion de Formulario Login/Creacion de usuario

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatOption, MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOption
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  isRegistering = false;
 
+  toggleRegister() {
+    this.isRegistering = !this.isRegistering;
+  }
+
+  login() {
+    // Lógica para iniciar sesión
+  }
+
+  register() {
+    // Lógica para registrar usuario
+  }
 }
