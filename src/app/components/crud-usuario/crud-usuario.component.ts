4060234457a1c765d63996d8d55e32b1a3a08ac0
Omar Yagual <its.oyaza20@hotmail.com>
2025-05-16 11:14:42 -0500

    Cambios en el CRUDUsuario Formulario Reactivo/Uso de Material

--- src/app/components/crud-usuario/crud-usuario.component.ts
+++ src/app/components/crud-usuario/crud-usuario.component.ts
@@ -1,26 +1,198 @@
 // filepath: src/app/components/crud-usuario/crud-usuario.component.ts
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import { Usuario } from '../../models/Usuario';
+import { FormBuilder, FormGroup, ReactiveFormsModule,Validators} from '@angular/forms';
 import { ServUsuarioService } from '../../services/serv-usuario.service';
+
+//Material Angular 
+
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator'; // Para paginación
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button'; // Para botones
+import { MatIconModule } from '@angular/material/icon'; // Para íconos
+import { MatInputModule } from '@angular/material/input'; // Para inputs
+import { MatFormFieldModule } from '@angular/material/form-field'; // Para campos de formulario
+import { MatSelectModule } from '@angular/material/select'; // Para selectores
+
+
 
 @Component({
   selector: 'app-crud-usuario',
-  imports: [CommonModule, HttpClientModule],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    //Validators,
+    MatTableModule,
+    MatPaginatorModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatSelectModule
+
+  ],
   templateUrl: './crud-usuario.component.html',
   styleUrl: './crud-usuario.component.css'
 })
-export class CrudUsuarioComponent {
-  usuarios: Usuario[] = [];
+export class CrudUsuarioComponent implements OnInit, AfterViewInit{
 
-  constructor(private usuarioService: ServUsuarioService) {
-    this.cargarUsuarios();
+  // FormGroup para manejar el formulario reactivo
+  form!: FormGroup;
+  
+  // Bandera para saber si estamos en modo edición
+   isEditMode: boolean = false;
+  
+  // ID de la película actualmente en edición
+   currentId!: number;
+
+  // Columnas a mostrar en la tabla
+  displayedColumns: string[] = [
+    'nombre',
+    'apellido',
+    'cedula',
+    'usuario',
+    'tipo_usuario',
+    'genero',
+    'direccion',
+    'contrasenia',
+    'actions'
+  ];
+
+  // Fuente de datos para la tabla
+  dataSource = new MatTableDataSource<Usuario>();
+
+  // Método del ciclo de vida: después de inicializar la vista
+  ngAfterViewInit() {
+    // Asigna el paginador a la tabla
+    this.dataSource.paginator = this.paginator;
   }
 
-  cargarUsuarios() {
-    this.usuarioService.getUsuarios().subscribe((data) => {
-      this.usuarios = data;
+  // Referencia al paginador de la tabla
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  
+
+
+  constructor(
+    private usuarioService: ServUsuarioService,
+    private fb: FormBuilder
+    ) {}
+
+  // Método del ciclo de vida: inicialización del componente
+  ngOnInit(): void {
+    this.cargarUsuarios(); // Carga inicial de datos
+
+    this.form = this.fb.group({
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      cedula: ['', [Validators.required, Validators.pattern(/^\d{10}$/)]], // 10 dígitos
+      usuario: ['', [Validators.required, Validators.minLength(4)]],
+      tipo_usuario: ['', Validators.required],
+      genero: ['', Validators.required],
+      direccion: ['', [Validators.required, Validators.minLength(5)]],
+      contrasenia: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+    
+  }
+
+
+  cargarUsuarios():void {
+    this.usuarioService.getUsuarios().subscribe((data:Usuario[]) => {
+      this.dataSource.data = data;
     });
   }
+  // Búsqueda de Usuario
+  // search(cajaTexto: HTMLInputElement) {
+  //   if (cajaTexto.value) {
+  //     // Si hay texto, busca películas que coincidan
+  //     this.miServicio
+  //       .getMoviesSearch(cajaTexto.value)
+  //       .subscribe((data: Pelicula[]) => {
+  //         this.dataSource.data = data;
+  //       });
+  //   } else {
+      
+  //     this.cargarTodasPeliculas();
+  //   }
+  // }
+
+  // Elimina una película con confirmación
+  // eliminar(pelicula: Pelicula) {
+  //   const confirmado = confirm(
+  //     `Está seguro de que desea eliminar la película ${pelicula.title}?`
+  //   );
+  //   if (confirmado) {
+  //     this.miServicio.deleteMovie(pelicula).subscribe({
+  //       next: () => {
+  //         alert('La película ha sido eliminada correctamente.');
+  //         this.cargarTodasPeliculas(); // Recarga la lista
+  //       },
+  //       error: (err) => {
+  //         console.error('Error al eliminar la película:', err);
+  //         alert('Ocurrió un error al intentar eliminar la película.');
+  //       },
+  //     });
+  //   }
+  // }
+
+  // Prepara el formulario para editar una película
+  // editar(peli: Pelicula) {
+  //   this.isEditMode = true; // Activa modo edición
+  //   this.currentId = peli.id; // Guarda el ID
+
+    // Llena el formulario con los datos de la película
+    // this.form.setValue({
+    //   title: peli.title,
+    //   genre: peli.genre,
+    //   releaseDate: peli.releaseDate,
+    //   budget: peli.budget,
+    //   poster: peli.poster ? peli.poster : '', // Manejo de valor nulo
+    //   rating: peli.rating,
+    //   isAvailable: peli.isAvailable,
+  //   // });
+  // }
+
+  // Limpia el formulario y desactiva el modo edición
+  // clearForm() {
+  //   this.form.reset({
+  //     title: '',
+  //     genre: '',
+  //     releaseDate: '',
+  //     budget: 0,
+  //     poster: '',
+  //     rating: '',
+  //     isAvailable: false,
+  //   });
+  //   this.isEditMode = false;
+  //   this.currentId = 0;
+  // }
+  // Maneja el envío del formulario (crear/actualizar)
+  // onSubmit() {
+  //   if (this.form.invalid) { // Verifica validaciones
+  //     alert('Por favor, complete todos los campos obligatorios.');
+  //     return;
+  //   }
+
+  //   let peliculaAGuardar: Pelicula = this.form.value;
+
+  //   if (this.isEditMode) {
+  //     // Modo edición: actualiza la película existente
+  //     peliculaAGuardar.id = this.currentId;
+
+  //     this.miServicio
+  //       .editMovie(peliculaAGuardar)
+  //       .subscribe((pelculaeditada) => {
+  //         alert('La película ha sido actualizada correctamente.');
+  //         this.cargarTodasPeliculas(); // Recarga la lista
+  //       });
+  //   } else {
+  //     // Modo creación: añade nueva película
+  //     this.miServicio.addMovie(peliculaAGuardar).subscribe(() => {
+  //       alert('La película ha sido guardada correctamente.');
+  //       this.cargarTodasPeliculas(); // Recarga la lista
+  //     });
+  //   }
+  // }
 }
--- src/app/services/serv-usuario.service.ts
+++ src/app/services/serv-usuario.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Usuario } from '../models/Usuario';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +9,18 @@ import { Observable } from 'rxjs';
 export class ServUsuarioService {
   private url = 'http://localhost:3000/usuario'; // Cambia la URL si es necesario
 
-  constructor(private http: HttpClient) {}
+  constructor(private httpcliente: HttpClient) {}
 
   getUsuarios(): Observable<Usuario[]> {
-    return this.http.get<Usuario[]>(this.url);
+    return this.httpcliente.get<Usuario[]>(this.url);
   }
+
+  // updateUsuario(usuario: Usuario): Observable<Usuario> {
+  // return this.httpcliente.put<Usuario>(`${this.url}/${usuario.id}`, usuario);
+  // }
+
+  // deleteUsuario(id: number): Observable<any> {
+  // return this.httpcliente.delete(`${this.url}/${id}`);
+  // }
+
 }
