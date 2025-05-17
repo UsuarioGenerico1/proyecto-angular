7c58d14771ca8b16e3f2432e1d17849e32fb6d4a
How Good <quebien2002@gmail.com>
2025-05-17 15:26:11 -0500

    tabla dinamica completada

--- src/app/components/crud-foro/crud-foro.component.ts
+++ src/app/components/crud-foro/crud-foro.component.ts
@@ -3,6 +3,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatDialog} from '@angular/material/dialog';
 import { VtnModalComponent } from '../../shared/vtn-modal/vtn-modal.component';
 import { TablaDinamicaComponent } from '../../shared/tabla-dinamica/tabla-dinamica.component';
+import { foro } from '../../models/foro';
+import { ServForoService } from '../../services/serv-foro.service';
 
 @Component({
   selector: 'app-crud-foro',
@@ -11,24 +13,41 @@ import { TablaDinamicaComponent } from '../../shared/tabla-dinamica/tabla-dinami
   styleUrl: './crud-foro.component.css'
 })
 export class CrudForoComponent {
-displayedColumns =['id', 'title' ,'genre','releaseDate'];
+admi:boolean = true;
 
+discuciones:foro[]=[];
+displayedColumns=[ 'titulo','contenido','id','acciones'];
+data: any[]=[];
 
+constructor(private servicio:ServForoService){
 
-
-isModalOpen = false;
-
-  openModal() {
-    this.isModalOpen = true;
-  }
-
-  cerrarModal() {
-    //aqui se puede agregar codigo necesario para cuando se da clik en cancelar
-    this.isModalOpen = false;
-  }
-
-confirmarAccion() {
-  //aqui se puede agregar codigo necesario cuando se acepta
-  this.isModalOpen = false;
 }
+ngOnInit():void{
+    this.cargarDiscuciones();
+    this.data=this.discuciones;
+}
+
+cargarDiscuciones():void{
+    //lamar al metodo del serivico 
+  this.servicio.getMovie().subscribe((data:foro[])=>{
+    this.discuciones=data;
+  });
+}
+
+//funciones para la table
+manejarEdicion(item: foro): void {
+  console.log('Editar:', item);
+  alert(`Boton presionado Editar: ${item.titulo}`);
+}
+
+manejarBorrado(item: any): void {
+  console.log('Borrar:', item);
+}
+
+
+// funciones para la  ventana modal  
+isModalOpen = false;
+  openModal() {this.isModalOpen = true;}
+  cerrarModal() {this.isModalOpen = false;}
+  confirmarAccion() {this.isModalOpen = false;}
 }
--- src/app/shared/tabla-dinamica/tabla-dinamica.component.ts
+++ src/app/shared/tabla-dinamica/tabla-dinamica.component.ts
@@ -1,15 +1,17 @@
-import { AfterViewInit, Component, input, OnInit, viewChild, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, input, OnInit, Output, viewChild, ViewChild } from '@angular/core';
 import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-tabla-dinamica',
-  imports: [MatTableModule,MatPaginatorModule],
+  imports: [MatTableModule,MatPaginatorModule,MatButtonModule],
   templateUrl: './tabla-dinamica.component.html',
   styleUrl: './tabla-dinamica.component.css'
 })
 export class TablaDinamicaComponent<T> implements OnInit{
- 
+  @Output() edit = new EventEmitter<T>();
+  @Output() delete = new EventEmitter<T>();
   displayedColumns = input.required<string[]>();
   data = input.required<T[]>();
   dataSource = new MatTableDataSource<T>();
@@ -17,6 +19,14 @@ export class TablaDinamicaComponent<T> implements OnInit{
   ngOnInit(): void {
     this.dataSource.data=this.data();
     this.dataSource.paginator = this._paginator();
+  }
+
+  onEdit(element: T): void {
+    this.edit.emit(element); // Puedes manejarlo como booleano si lo deseas
+  }
+
+  onDelete(element: T): void {
+    this.delete.emit(element);
   }
 }
 
